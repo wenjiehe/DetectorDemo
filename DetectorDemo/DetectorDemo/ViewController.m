a0a173eaad122b4048c95382316855d2c203554c
YNET <ynet@YNETdeMacBook-Pro.local>
2021-10-18 11:48:56 +0800

    ..

--- DetectorDemo/DetectorDemo/ViewController.m
+++ DetectorDemo/DetectorDemo/ViewController.m
@@ -7,8 +7,9 @@
 
 #import "ViewController.h"
 #import <CoreImage/CIDetector.h>
-#import "VisionKitManager.h"
 #import "VisionManager.h"
+#import <VisionKit/VisionKit.h>
+#import <Photos/Photos.h>
 
 typedef NS_ENUM(NSUInteger, faceType) {
     faceTypeLeftEye,
@@ -17,7 +18,9 @@ typedef NS_ENUM(NSUInteger, faceType) {
     faceTypeFace,
 };
 
-@interface ViewController ()
+@interface ViewController ()<VNDocumentCameraViewControllerDelegate>
+
+@property(nonatomic,strong)VNDocumentCameraViewController *dcVC;
 
 @end
 
@@ -49,10 +52,10 @@ typedef NS_ENUM(NSUInteger, faceType) {
 //    [self analysisRectangle];
     
     //扫描文档转化为图片
-//    [VisionKitManager cameraScanDocument:self];
+    [self cameraScanDocument:self];
     
     //识别图片中的文字
-    [VisionManager imageDetectionManager:nil];
+//    [VisionManager imageDetectionManager:nil];
 }
 
 /**
@@ -299,6 +302,98 @@ typedef NS_ENUM(NSUInteger, faceType) {
     }
 }
 
+
+/*
+    使用VNDocumentCameraViewController类需要在plist文件中添加以下两个key:
+    NSPhotoLibraryAddUsageDescription
+    NSCameraUsageDescription
+    NSPhotoLibraryUsageDescription 保存图片到相册需要这个
+ */
+- (void)cameraScanDocument:(UIViewController *)viewController{
+    if (@available(iOS 13.0, *)) {
+        if (!VNDocumentCameraViewController.supported) {
+            UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"当前设备不支持" preferredStyle:UIAlertControllerStyleAlert];
+            [alertVC addAction:[UIAlertAction actionWithTitle:@"我知道了" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                            
+            }]];
+            [viewController presentViewController:alertVC animated:YES completion:^{
+                            
+            }];
+        }else{
+            self.dcVC = [[VNDocumentCameraViewController alloc] init];
+            self.dcVC.delegate = self;
+            [self presentViewController:self.dcVC animated:YES completion:^{
+                            
+            }];
+        }
+    } else {
+        
+    }
+}
+
+#pragma mark -- tools
+- (void)writeImage:(NSMutableArray<UIImage *> *)imgs{
+    __block NSMutableArray<NSString *> *localIdentifierMtbAry = [NSMutableArray new];
+    //获取我的相薄里面的所有相册
+    PHFetchOptions *fetOptions = [[PHFetchOptions alloc] init];
+    fetOptions.includeAssetSourceTypes = PHAssetSourceTypeUserLibrary;
+    PHFetchResult *result = [PHCollectionList fetchTopLevelUserCollectionsWithOptions:fetOptions];
+    [result enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        PHAssetCollection *assetCollection = obj;
+        NSLog(@"localizedTitle = %@", assetCollection.localizedTitle);
+        if ([assetCollection.localizedTitle isEqualToString:@"QQ"]) { //选定目标相册
+            [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+                for (NSInteger i = 0; i < imgs.count; i++) {
+                    UIImage *image = imgs[i];
+                    PHAssetChangeRequest *request = [PHAssetChangeRequest creationRequestForAssetFromImage:image];
+                    //请求编辑相册
+                    PHAssetCollectionChangeRequest *collectionChangeRequest = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:assetCollection];
+                    //为Asset创建一个占位符，放到相册编辑请求中
+                    PHObjectPlaceholder *placeholder = [request placeholderForCreatedAsset];
+                    [collectionChangeRequest addAssets:@[placeholder]];
+                    [localIdentifierMtbAry addObject:placeholder.localIdentifier];
+                }
+            } completionHandler:^(BOOL success, NSError * _Nullable error) {
+                if (success) { //保存图片成功
+                    NSLog(@"保存图片成功");
+                    [self.dcVC dismissViewControllerAnimated:YES completion:^{
+                            
+                    }];
+                }else{ //保存图片失败
+                    NSLog(@"保存图片失败");
+                }
+            }];
+            *stop = YES;
+        }
+    }];
+}
+
+#pragma mark -- VNDocumentCameraViewControllerDelegate
+- (void)documentCameraViewController:(VNDocumentCameraViewController *)controller didFinishWithScan:(VNDocumentCameraScan *)scan API_AVAILABLE(ios(13.0)){
+    NSLog(@"scan.title = %@", scan.title);
+    NSMutableArray *imageMtbAry = [NSMutableArray new];
+    for (NSInteger i = 0; i < scan.pageCount; i++) {
+        UIImage *image = [scan imageOfPageAtIndex:i];
+        [imageMtbAry addObject:image];
+    }
+    if (imageMtbAry.count > 0) {
+//        [self writeImage:imageMtbAry];
+        [VisionManager imageDetectionManager:imageMtbAry];
+        [controller dismissViewControllerAnimated:YES completion:^{
+                
+        }];
+    }
+}
+
+- (void)documentCameraViewControllerDidCancel:(VNDocumentCameraViewController *)controller API_AVAILABLE(ios(13.0)){
+    [controller dismissViewControllerAnimated:YES completion:^{
+            
+    }];
+}
+
+- (void)documentCameraViewController:(VNDocumentCameraViewController *)controller didFailWithError:(NSError *)error API_AVAILABLE(ios(13.0)){
+    NSLog(@"fail error = %@", error);
+}
 
 
 @end
